ef10e2c7694d05f311697b55489d89b31c9e4c27
Wibowo Dwi Suwito <50950100+wibowodwi@users.noreply.github.com>
2026-09-22 15:12:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 window.SHOPMATE_CONFIG = {
   supabaseUrl: 'https://cmvwpwaipemdsjeayucp.supabase.co',
   supabaseAnonKey: 'sb_publishable_wAG9WXT44QOkIyxG0HO6OQ_W7VB2fRw',
-  whatsappNumber: '',
-  storeName: 'Toko Pade',
+  whatsappNumber: '6285183180626',
+  storeName: 'Toko Keripik Pade',
   // URL default: Supabase Edge Function chatbot setelah deploy. Bisa diganti dengan URL endpoint lain.
   chatbotEndpoint: 'https://cmvwpwaipemdsjeayucp.supabase.co/functions/v1/chatbot'
 };
